8e979c3d1c000bc6a7322a835e34899eab9bab23
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-03-26 22:14:04 -0600

    built quiz models

---
+++ multiple_choice/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Quiz(models.Model):
+    quiz_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.quiz_name
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    question_number = models.IntegerField()
+    question_text = models.TextField(null=True)
+
+    def __str__(self):
+        return self.question_number
+
+
+class Answer(models.Model):
+    correct_answer = models.CharField(max_length=25)
+    incorrect_answer_1 = models.CharField(max_length=25)
+    incorrect_answer_2 = models.CharField(max_length=25)
+    incorrect_answer_3 = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.correct_answer
