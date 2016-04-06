8895bb2040fc7cda337d7141ba16611568d7046a
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-04-06 13:23:47 -0600

    Edit Question model

--- multiple_choice/models.py
+++ multiple_choice/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     question_text = models.TextField(null=True)
 
     def __str__(self):
-        return self.question_number
+        return self.question_text
 
 
 class Answer(models.Model):
