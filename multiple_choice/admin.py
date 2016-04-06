6d2144079f3e721d0754c5f562443de22b64f804
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-04-06 13:16:58 -0600

    created admin

---
+++ multiple_choice/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Quiz, Question, Answer
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fields = ["quiz_name"]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["quiz", "question_number", "question_text"]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    fields = ["question", "correct_answer", "incorrect_answer_1", "incorrect_answer_2",
+                "incorrect_answer_3"]
+
+
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
