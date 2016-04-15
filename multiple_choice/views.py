9d9c60e24824da9619d31ff228215d89d99bc71b
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-04-14 21:50:46 -0600

    removed question template and condensed to quiz template

--- multiple_choice/views.py
+++ multiple_choice/views.py
@@ -8,17 +8,11 @@ from django.views import generic
 from .models import Quiz
 
 
-def quiz(request, quiz_id):
-    template_name = "multiple_choice_quiz.html"
-    active_quiz = get_object_or_404(Quiz, pk=quiz_id)
-    ctx = {'quiz': active_quiz}
-    return render(request, template_name, ctx)
-
-def question(request, quiz_id, question_id):
+def quiz(request, quiz_id, question_id):
     p = get_object_or_404(Quiz, pk=quiz_id)
     try:
         active_question = p.question_set.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     else:
-        return render(request, "multiple_choice_question.html", {'question': active_question})
+        return render(request, "multiple_choice_quiz.html", {'question': active_question})
