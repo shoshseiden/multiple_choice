0710a421b28af3cccf5e273f1541d11c2600cdc7
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-07-05 20:57:59 -0600

    Removed commented out calculations in models.py

--- multiple_choice/models.py
+++ multiple_choice/models.py
@@ -27,28 +27,3 @@ class Question(models.Model):
             total_number_of_questions = question_number + 1
 
         return total_number_of_questions
-
-"""
-    #Copied from calculations.py
-    def calculate_total_points(self):
-        total_points = 0
-
-        point_value = question_point_value
-        total_points += point_value
-
-        return self.total_points
-
-    def student_score(self):
-        student_score = 0
-        total_score = 0
-
-        total_score += student_score
-
-        return self.total_score
-
-    def calculate_percentage(self):
-        percentage = 0
-        percentage = 100 * (total_score / total_points)
-
-        return self.percentage
-"""
