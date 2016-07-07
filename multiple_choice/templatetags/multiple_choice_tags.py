7793ee50081cf284ef29307291496ad22c5cf18f
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-07-06 20:18:11 -0600

    Added Quiz model to templatetags

--- multiple_choice/templatetags/multiple_choice_tags.py
+++ multiple_choice/templatetags/multiple_choice_tags.py
@@ -3,11 +3,9 @@ from __future__ import division
 from django import template
 from django.core.urlresolvers import reverse
 
-from ..models import Question
+from ..models import Quiz, Question
 
 register = template.Library()
 
 
 @register.assignment_tag
-def question_tags(quiz):
-    pass
