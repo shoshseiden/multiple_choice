da298dce7b76236c1f8a38fa732f9e97abc32ece
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-07-19 20:42:54 -0600

    indented comment quotations

--- multiple_choice/models.py
+++ multiple_choice/models.py
@@ -22,12 +22,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-"""
+    """
     @property
     def get_max_question(self, max_question):
         max_question = self.objects.all().aggregate(Max("question_number"))
         return max_question
-"""
+    """
 
     def get_question_sequencing(self):
         all_questions = self.objects.all()
@@ -38,4 +38,4 @@ class Question(models.Model):
                 question_list.append(current_question)
                 return question_list
             else:
-                return None 
+                return None
