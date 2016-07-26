72d3733ff157c48a7f6625642bfbfa250a5d89db
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-07-25 20:23:15 -0600

    revised login views to work with assigned login information

--- multiple_choice/views.py
+++ multiple_choice/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, Http404
 from django import template
 from django.template.loader import get_template
 from django.template import RequestContext, loader
 from django.views import generic
 
+from .forms import StudentLoginForm, InstructorLoginForm
 from .models import Quiz, Question
 
 
@@ -23,4 +24,14 @@ def result_view(request, quiz_id):
     return render(request, "results.html", {'quiz': p})
 
 def login_view(request):
-    return render(request, "login.html")
+    if request.method=="POST":
+        student_form = StudentLoginForm(request.POST)
+        instructor_form = InstructorLoginForm(request.POST)
+        if student_form.is_valid():
+            return redirect("home")
+        if instructor_form.is_valid():
+            return redirect("home")
+    else:
+        student_form = StudentLoginForm()
+        instructor_form = InstructorLoginForm()
+    return render(request, "login.html", {"student_form": student_form, "instructor_form": instructor_form})
