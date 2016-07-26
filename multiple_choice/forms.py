48e62aeadea742ecddfeeeae222cd099188cdb35
Shosh Seiden <shosh.j.seiden@gmail.com>
2016-07-25 20:01:29 -0600

    setup login forms

---
+++ multiple_choice/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+class StudentLoginForm(forms.Form):
+
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
+
+        if username != "student":
+            raise forms.ValidationError("Incorrect username/password")
+        if password != "a":
+            raise forms.ValidationError("Incorrect username/password")
+        return self.cleaned_data
+
+
+class InstructorLoginForm(forms.Form):
+
+    username = froms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
+
+        if username != "instructor":
+            raise forms.ValidationError("Incorrect username/password")
+        if password != "b":
+            raise forms.ValidationError("Incorrect username/password")
+        return self.cleaned_data
