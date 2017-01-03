9ad48b4984609611f9777080b731c628bb9bdf75
Shosh Seiden <shosh.j.seiden@gmail.com>
2017-01-02 18:27:33 -0700

    created get_correct_answer in answer class

--- multiple_choice/models.py
+++ multiple_choice/models.py
@@ -52,6 +52,7 @@ class Answer(models.Model):
 
     question = models.ForeignKey(Question)
     answer_text = models.CharField(max_length=25)
+    quiz_total = models.IntegerField()
 
     def __str__(self):
         return self.answer_text
@@ -62,3 +63,13 @@ class Answer(models.Model):
             answer_list = []
             answer_list.append(self.answer_text)
             return answer_list
+
+        @property
+        def get_correct_answer(self):
+            quiz_total = 0
+            if self.answer_text = question.correct_answer:
+                quiz_total += question.question_point_value
+            else:
+                quiz_total += 0
+
+            return quiz_total
